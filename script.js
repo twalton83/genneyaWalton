beaf1f1ce14aefca25ad5d6d5b420d0a7c0c680f
Tatiana <tatianaw83@gmail.com>
2020-07-09 15:30:18 -0700

    fixed transition

--- script.js
+++ script.js
@@ -1,36 +1,18 @@
-const imageTop = document.querySelector('.topImage')
-const imageBottom = document.querySelector('.bottomImage')
+$('.carousel').carousel({
+    interval: 2000
+  })
 
-const mainImageCarousel = () => {
-    let i = 0
-    let nextImage; 
-    let currentImage = imageTop
-    const imageSrcs =["img/img-primary.jpg",
-    "img/img-secondary.jpg",
-    "img/img-1.jpg",
-    "img/img-2.jpg",
-    "img/img-3.jpg",
-    "img/img-4.jpg",
-    "img/img-5.jpg" ]
-        setInterval(function(){
-        if(i === imageSrcs.length){
-            i = 0
-        }
-            imageTop.src = imageSrcs[i]
-        // the first image needs to fade into the second
-            imageTop.style.opacity = 0
-            currentImage = imageBottom
-         
-            let nextImage = imageSrcs[i+1]
-            imageTop.src = nextImage
-         // then the first image needs to fade back in 
-            imageTop.style.opacity = 1
-            currentImage = imageTop
-        i++
-        }, 3000) 
-    
+
+const header = document.querySelector('.navbar')
+console.log(header)
+console.log(header.style)
+
+function changeHeaderColor(){
+    if(window.scrollY > 10){
+        header.classList.add('scrolledNav')
+    } else {
+        header.classList.remove('scrolledNav')
     }
-    
+}
 
-
-document.addEventListener('DOMContentLoaded', mainImageCarousel)
+document.addEventListener('scroll', changeHeaderColor)
